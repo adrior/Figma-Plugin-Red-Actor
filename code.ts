9f14064924f77ab9ead42a1ba79c76a30885640f
Anthony Shein <anton.adrior@gmail.com>
2023-04-06 18:20:16 +0300

    Working draft

--- code.ts
+++ code.ts
@@ -6,7 +6,7 @@
 // full browser environment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+figma.showUI(__html__, { themeColors: true, width: 400, height: 600 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
@@ -14,38 +14,76 @@ figma.showUI(__html__);
 figma.ui.onmessage = (msg) => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === "create-rectangles") {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
+  // if (msg.type === "create-rectangles") {
+  //   const nodes: SceneNode[] = [];
+  //   for (let i = 0; i < msg.count; i++) {
+  //     const rect = figma.createRectangle();
+  //     rect.x = i * 150;
+  //     rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+  //     figma.currentPage.appendChild(rect);
+  //     nodes.push(rect);
+  //   }
+  //   figma.currentPage.selection = nodes;
+  //   figma.viewport.scrollAndZoomIntoView(nodes);
+  // }
 
-  if (msg.type === "find-texts") {
+  function findTexts() {
     let nodes = figma.currentPage.findAllWithCriteria({ types: ["TEXT"] });
-    console.log(nodes);
     let texts = nodes.map((textLayer) => {
-      console.log(textLayer);
       return {
-        name: textLayer.name,
+        key: textLayer.name,
         content: textLayer.characters,
       };
     });
-    console.log(texts);
     figma.ui.postMessage({ type: "text-received", content: texts });
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
   }
 
+  if (msg.type === "find-texts") {
+    findTexts();
+  }
+
   if (msg.type === "cancel") {
     figma.closePlugin();
   }
+
+  if (msg.type === "find-on-page") {
+    let nodes = figma.currentPage.findAll(
+      (n) =>
+        n.type === "TEXT" && n.name === msg.key && n.characters === msg.content
+    );
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+
+  if (msg.type === "replace-other") {
+    console.log(msg);
+    let nodes = figma.currentPage.findAll(
+      (n) =>
+        n.type === "TEXT" && n.name === msg.key && n.characters !== msg.content
+    );
+    nodes.forEach((n) => {
+      n.characters = msg.content;
+    });
+
+    findTexts();
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+  }
+
+  async function loadFonts() {
+    let nodes = figma.currentPage.findAllWithCriteria({ types: ["TEXT"] });
+    nodes.forEach(async (node) => {
+      await Promise.all(
+        node
+          .getRangeAllFontNames(0, node.characters.length)
+          .map(figma.loadFontAsync)
+      );
+    });
+  }
+
+  loadFonts();
 
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
